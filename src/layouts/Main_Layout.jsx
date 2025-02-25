f29a5eac83821e1442fb3bafb4c61ed5ceb70111
AbdElRahman <abdoxawd@gmail.com>
2025-02-25 19:18:34 +0200

    main_layout

--- src/layouts/Main_Layout.jsx
+++ src/layouts/Main_Layout.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 const Main_Layout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>nav</div>
+      <Outlet />
+      <div>footer</div>
+    </div>
+  );
 };
 
 export default Main_Layout;
