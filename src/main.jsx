35c8cec6c8616926c347b080bf2bc9b0d6372ae0
AbdElRahman <abdoxawd@gmail.com>
2025-02-25 09:59:46 +0200

    add shadcn/ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./Store/redux";
+import { store } from "./Store/store";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
