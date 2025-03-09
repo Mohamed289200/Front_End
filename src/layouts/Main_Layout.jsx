f84db58d2e1fc32f3f2cc10ce1cd1814cfddb7e1
AbdElRahman <abdoxawd@gmail.com>
2025-03-09 17:37:41 +0200

    fix:main layout

--- src/layouts/Main_Layout.jsx
+++ src/layouts/Main_Layout.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 const Main_Layout = () => {
-  return <div><Outlet/></div>;
+  return (
+    <div>
+      <div>ana el main layout ya wald</div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Main_Layout;
