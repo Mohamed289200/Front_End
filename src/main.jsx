8bfc5eab14ef2ee668c0d2dc3cb4e306d3ff6493
Mohamed289200 <mohamedabd092002@gmail.com>
2025-03-12 17:13:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./Store/store";
+import { store } from "./store/store";
 import App from "./App";
 import React from "react";
 
